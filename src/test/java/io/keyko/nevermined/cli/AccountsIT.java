b30a993b23666cf44fa02fe2f9e490496637a661
Aitor <1726644+aaitor@users.noreply.github.com>
2020-08-14 17:07:18 +0200

    adding option for running IT tests

--- src/test/java/io/keyko/nevermined/cli/AccountsIT.java
+++ src/test/java/io/keyko/nevermined/cli/AccountsIT.java
@@ -5,9 +5,11 @@ import com.typesafe.config.ConfigFactory;
 import io.keyko.nevermined.NeverminedCLI;
 import io.keyko.nevermined.cli.models.CommandResult;
 import io.keyko.nevermined.cli.models.exceptions.CLIException;
+import org.junit.Ignore;
 import org.junit.Test;
 import picocli.CommandLine;
 
+import static io.keyko.nevermined.cli.TestsBase.TESTS_CONFIG_FOLDER;
 import static org.junit.Assert.assertTrue;
 
 public class AccountsIT {
@@ -17,14 +19,15 @@ public class AccountsIT {
     @Test
     public void newAccount() throws CLIException {
         String[] args= {"accounts", "new", "-p", "1234", "-d", "/tmp"};
-        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(), args);
+        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(TESTS_CONFIG_FOLDER), args);
         assertTrue(result.isSuccess());
     }
 
     @Test
+    @Ignore
     public void newAccountDefault() throws CLIException {
         String[] args= {"accounts", "new", "-p", "1234", "-m"};
-        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(), args);
+        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(TESTS_CONFIG_FOLDER), args);
         assertTrue(result.isSuccess());
 
     }
@@ -32,14 +35,15 @@ public class AccountsIT {
     @Test
     public void listAccounts() throws CLIException {
         String[] args= {"accounts", "list"};
-        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(), args);
+        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(TESTS_CONFIG_FOLDER), args);
         assertTrue(result.isSuccess());
     }
 
     @Test
     public void balanceAccount() throws CLIException {
         String[] args= {"accounts", "balance", config.getString("account.main.address")};
-        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(), args);
+        CommandResult result = (CommandResult) CommandLine.call(new NeverminedCLI(TESTS_CONFIG_FOLDER), args);
+
         assertTrue(result.isSuccess());
     }
 
