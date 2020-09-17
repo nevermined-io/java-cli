267a804655abf8fe714683157fdcfc89a8188c22
Aitor <1726644+aaitor@users.noreply.github.com>
2020-09-17 12:18:45 +0200

    Adding mkdir trying to fix some initialization problems

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ env
 
 $ROOT_PATH="/"
 
+mkdir -p /root/.local/share/nevermined-cli/networks/
 envsubst < $ROOT_PATH/src/main/resources/application.template > /root/.local/share/nevermined-cli/application.conf
 envsubst < $ROOT_PATH/src/main/resources/log4j2.template > /root/.local/share/nevermined-cli/log4j2.properties
 envsubst < $ROOT_PATH/src/main/resources/networks/networks.template > /root/.local/share/nevermined-cli/networks/${KEEPER_NETWORK_NAME}.conf
